2be65c5f6c6b4d16a884d1b43f952538abd610e2
Grover <grover.gonzalez@ucb.edu.bo>
2023-06-20 10:39:36 -0400

    Realizando la publicacion para amigos

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,8 @@
 
 const textopublicar = document.getElementById('texto-publicacion');
+const textoPublicarAmigos = document.getElementById('texto-publicacion-amigos');
 const form = document.getElementById('Publicacion-form');
+const formAmigos = document.getElementById('Publicacion-Amigos-form');
 const div = document.getElementById('resultado-div');
 
 form.addEventListener("submit", (event) => {
@@ -9,4 +11,12 @@ form.addEventListener("submit", (event) => {
   const textoApublicar = textopublicar.value;
 
   div.innerHTML = "<p>" + textoApublicar + "</p>";
+});
+
+formAmigos.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const textoApublicarAmigos = textoPublicarAmigos.value;
+
+  div.innerHTML = "<p>" + "Solo Para Amigos" + textoApublicarAmigos + "</p>";
 });
